8ca5b64a28592ffdd3e6f62f7976c181dcfbbd2d
Camila Saucedo <camilasaucedo62@gmail.com>
2023-11-25 01:18:18 +0100

    Camila
    Actualizacion

---
+++ tukiservicios/src/main/java/com/example/tukiservicios/tukiservicios/models/supplier/ProfessionEnum.java
@@ -0,0 +1,10 @@
+package com.example.tukiservicios.tukiservicios.models.supplier;
+
+
+public enum ProfessionEnum {
+    GASISTA,
+    PLOMERO,
+    ELECTRICISTA
+
+
+}
--- tukiservicios/src/main/java/com/example/tukiservicios/tukiservicios/models/supplier/SupplierEntity.java
+++ tukiservicios/src/main/java/com/example/tukiservicios/tukiservicios/models/supplier/SupplierEntity.java
@@ -1,6 +1,7 @@
-package com.example.tukiservicios.tukiservicios.models;
+package com.example.tukiservicios.tukiservicios.models.supplier;
 
-import com.example.tukiservicios.tukiservicios.models.SupplierEntityEnum;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
@@ -9,34 +10,26 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
-@Entity
 @AllArgsConstructor
+@Entity
 @NoArgsConstructor
-
-
 public class SupplierEntity {
-
     @Id
-    @Column(name = "id")
-    private int id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @NonNull
     @Column(name="supplier_name")
     private String supplierName;
     @NonNull
-    @Column(name = "foto")
     private String foto;
     @NonNull
     @Email
-    @Column(name = "email")
     private String email;
     @NonNull
-    @Column(name="password")
     private String password;
     @NonNull
-    @Column(name="resume")
     private String resume;
     @NonNull
-    @Column(name = "price")
     private float price;
     @NonNull
     @Column(name = "contact_number")
@@ -44,18 +37,13 @@ public class SupplierEntity {
     @Column(name = "work_status")
     private String workStatus;
     @NonNull
-    @Id
-    @Column(name = "id_user")
-    private int idUser;
-    @Id
+    @OneToOne(fetch = FetchType.EAGER, targetEntity = ProfessionEnum.class, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "supplier_service", joinColumns = @JoinColumn(name = "id"), inverseJoinColumns = @JoinColumn(name = "id_service"))
+    private Set<ProfessionEnum> profession;
     @NonNull
-    @Column(name = "id_service")
-    private int idService;
-
-    @NonNull
-    @OneToOne
-    @Enumerated(EnumType.STRING)
-    private SupplierEntityEnum profession;
+    @OneToMany(fetch = FetchType.EAGER, targetEntity = UserEntity.class, cascade = CascadeType.PERSIST)
+    @JoinTable(name = "supplier_user", joinColumns = @JoinColumn(name = "id"),  inverseJoinColumns = @JoinColumn(name = "id_user"))
+    private  Set<UserEntity> clientes;
 
 
 }
