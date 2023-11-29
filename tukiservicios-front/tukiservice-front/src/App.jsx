09f3826f265f1d5ef138e904bf613206bdbc07eb
Pao Sotomayor <126527997+PaoSotomayor@users.noreply.github.com>
2023-11-29 14:14:10 -0500

    Rutas agregadas para las 3 vistas

--- tukiservicios-front/tukiservice-front/src/App.jsx
+++ tukiservicios-front/tukiservice-front/src/App.jsx
@@ -1,19 +1,25 @@
 import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<h1>Index</h1>}> </Route>
-        <Route path='/crearSupplier' element={<h1>Registrarse como PROVEEDOR</h1>}> </Route>
-        <Route path='/listaSupplier' element={<h1>Ver listado de PROVEEDORES</h1>}> </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<h3>Index</h3>}> </Route>
+          <Route path='/crearSupplier' element={<h3>Registrarse como PROVEEDOR</h3>}> </Route>
+          <Route path='/listaSupplier' element={<h3>Ver listado de PROVEEDORES</h3>}> </Route>
+          <Route path='/crearUser' element={<h3>Registrarse como USUARIO</h3>}> </Route>
+          <Route path='/listaUser' element={<h3>Ver listado de USUARIOS</h3>}> </Route>
+          <Route path='/login' element={<h3>LOGIN</h3>}> </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- tukiservicios-front/tukiservice-front/src/components/Header.jsx
+++ tukiservicios-front/tukiservice-front/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
@@ -6,6 +7,9 @@ export default function Header() {
             <Link to={"/"}>Index</Link>
             <Link to={"/crearSupplier"}>Registrarse como PROVEEDOR</Link>
             <Link to={"/listaSupplier"}>listado de PROVEEDORES</Link>
+            <Link to={"/crearUser"}>Registrarse como USUARIO</Link>
+            <Link to={"/listaUser"}>listado de USUARIOS</Link>
+            <Link to={"/login"}>LOGIN</Link>
 
         </nav>
     )
