c9c3f9ad7a6e04cd6bdd03a958bd1c53be82a8fc
ProyectoTeam5 <151761741+ProyectoTeam5@users.noreply.github.com>
2023-11-29 01:05:02 +0100

    Delete tukiservicios

--- tukiservicios/src/main/java/com/example/tukiservicios/tukiservicios/repositories/ServiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tukiservicios.tukiservicios.repositories;
-
-import com.example.tukiservicios.tukiservicios.models.supplier.Service;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ServiceRepository extends CrudRepository<Service,Long> {
-
-
-}
