c1479c13c68248b1703bf2a375040dc9ae40d5b0
Gustavo M <gustavoxmv11@gmail.com>
2023-11-29 20:48:12 -0400

    Cambio_3

--- tukiservice/tukiservice/src/main/java/com/tukiservice/security/config/Supplier/SecurityConfig.java
+++ tukiservice/tukiservice/src/main/java/com/tukiservice/security/config/Supplier/SecurityConfig.java
@@ -10,27 +10,38 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 
+
 public class SecurityConfig {
 
     @Bean
+    @SuppressWarnings("removal")
 public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
 return httpSecurity
 
-
         .authorizeHttpRequests(authRequest->
-
                 authRequest
                         .requestMatchers("/api/supplier").permitAll()
                         .anyRequest().authenticated())
-        .formLogin(Customizer.withDefaults())
+        .formLogin()
+        .successHandler(successHandler())
+        .permitAll()
+        .and()
+        .httpBasic()
+        .and()
         .build();
 
 }
 
+    public AuthenticationSuccessHandler successHandler(){
+        return ((request, response, authentication) -> {
+            response.sendRedirect("/api/supplier");
+        });
+    }
 }
